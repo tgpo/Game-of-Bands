66f3c44fd3857dccac6618fea657f1a1dbbef2a2
orphoneus <orphoneus@alpenjodel.de>
2013-11-12 00:17:59 +0100

    Split 'includes' directory into 'lib' and 'src' directory.

--- src/secrets_test.php
+++ src/secrets_test.php
@@ -2,8 +2,8 @@
 // Secrets on the server side -- passwords, user names, etc
 // These are for your local development environment
 
-$mysql_user     = 'test';
-$mysql_password = 'hello';
+$mysql_user     = 'root';
+$mysql_password = 'root';
 
 // Reddit user for test purposes.
 $reddit_user     = 'test';
