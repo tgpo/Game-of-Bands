18149db9c8c1e1f1d5595b374379d8a13cc5bf3e
orphoneus <orphoneus@alpenjodel.de>
2013-11-12 22:44:30 +0100

    Update the Hall of Fame to the new style.

--- src/view_fame.php
+++ src/view_fame.php
@@ -4,62 +4,60 @@
 </div>
 
 <?php
-// BEST SONGS
+  require_once('query.php');
 
-// orders the table by votes, then selects the first entry of each group. Still needs to be modified to display equal maximums.
-$result = mysql_query("SELECT * FROM (SELECT * FROM songs WHERE votes IS NOT NULL ORDER BY votes DESC) AS s GROUP BY round DESC") or die(mysql_error());
+  $db = mysqli_connect();
 
+  // BEST SONGS
+  // Orders the table by votes, then selects the first entry of each group.
+  // Still needs to be modified to display equal maximums.
+  $songs = $db->query('SELECT * FROM (SELECT * FROM songs WHERE votes IS NOT NULL ORDER BY votes DESC) AS s GROUP BY round DESC');
+  display_songs($songs);
 
-// dispays the standard song list
-include 'displaySongList.php';
+  // Display individual winners
+  function display_single_bandit($result) {
+    echo "<table>";
+  	echo "<tr><th>Round</th><th>Song Title</th><th>Votes</th><th>Winner</th><th>Votes</th></tr>";
+    while ($row = mysql_fetch_array($result)) {
+  		echo "<tr>";
+  	  echo "<td>".a_round($row['number'],$row['round'])."</td>";
+  	  echo "<td>".a_song ($row)."</td>";
+  	  echo "<td>".$row['votes']."</td>";
+  	  echo "<td>".a_bandit($row['winner'])."</td>";
+  	  echo "<td>".$row['winnervotes']."</td>";
+  	  echo "</tr>";
+    }
+    echo "</table>";
+  }
+  
+  // BEST MUSICIANS
+ 
+  // Orders the table by votes, then selects the first entry of each group.
+  // Still needs to be modified to display equal maximums.
+  $result = $db->query("SELECT id, name, round, votes, music AS winner, musicvote AS winnervotes FROM (SELECT * FROM songs WHERE votes IS NOT NULL ORDER BY musicvote DESC) AS s GROUP BY round DESC");
+  
+  echo "<div class='float'>";
+    echo "<div class='header'>";
+    echo "The Game of Bands Hall of Fame - Winning Musicians";
+    echo "</div>";
+    display_single_bandit($result);
+  echo "</div>";
 
-// BEST MUSICIANS
-
-// orders the table by votes, then selects the first entry of each group. Still needs to be modified to display equal maximums.
-$result = mysql_query("SELECT id, name, round, votes, music AS winner, musicvote AS winnervotes FROM (SELECT * FROM songs WHERE votes IS NOT NULL ORDER BY musicvote DESC) AS s GROUP BY round DESC") or die(mysql_error());
-
-echo "<div class='float'>";
-
-// displays a brief header before table
-echo "<div class='header'>";
-echo "The Game of Bands Hall of Fame - Winning Musicians";
-echo "</div>";
-
-// BEST LYRICISTS
-
-// dispays the standard song list
-include 'displaySingleBanditSongList.php';
-echo "</div>";
-
-// orders the table by votes, then selects the first entry of each group. Still needs to be modified to display equal maximums.
-$result = mysql_query("SELECT id, name, round, votes, lyrics AS winner, lyricsvote AS winnervotes FROM (SELECT * FROM songs WHERE votes IS NOT NULL ORDER BY lyricsvote DESC) AS s GROUP BY round DESC") or die(mysql_error());
-
-echo "<div class='float'>";
-
-
-// displays a brief header before table
-echo "<div class='header'>";
-echo "The Game of Bands Hall of Fame - Winning Lyricists";
-echo "</div>";
-
-// dispays the standard song list
-include 'displaySingleBanditSongList.php';
-
-echo "</div>";
-// BEST VOCALISTS
-
-// orders the table by votes, then selects the first entry of each group. Still needs to be modified to display equal maximums.
-$result = mysql_query("SELECT id, name, round, votes, vocals AS winner, vocalsvote AS winnervotes FROM (SELECT * FROM songs WHERE votes IS NOT NULL ORDER BY vocalsvote DESC) AS s GROUP BY round DESC") or die(mysql_error());
-
-echo "<div class='float'>";
-
-// displays a brief header before table
-echo "<div class='header'>";
-echo "The Game of Bands Hall of Fame - Winning Vocalists";
-echo "</div>";
-
-// dispays the standard song list
-include 'displaySingleBanditSongList.php';
-
-echo "</div>";
+  // BEST LYRICISTS
+  $result = $db->query("SELECT id, name, round, votes, lyrics AS winner, lyricsvote AS winnervotes FROM (SELECT * FROM songs WHERE votes IS NOT NULL ORDER BY lyricsvote DESC) AS s GROUP BY round DESC");
+  echo "<div class='float'>";
+    echo "<div class='header'>";
+    echo "The Game of Bands Hall of Fame - Winning Lyricists";
+    echo "</div>";
+    display_single_bandit($result);
+  echo "</div>";
+  
+  // BEST VOCALISTS
+  $result = $db->query("SELECT id, name, round, votes, vocals AS winner, vocalsvote AS winnervotes FROM (SELECT * FROM songs WHERE votes IS NOT NULL ORDER BY vocalsvote DESC) AS s GROUP BY round DESC");
+  echo "<div class='float'>";
+    echo "<div class='header'>";
+    echo "The Game of Bands Hall of Fame - Winning Vocalists";
+    echo "</div>";
+    display_single_bandit($result);
+  echo "</div>";
 ?>
