fa2f90d25084b62fd3214655510d7364f2bd2cd0
tgpo <tgpotgpo@gmail.com>
2013-11-14 13:24:14 -0500

    Fixed secret folder location & updated data display

--- admin/songlist.php
+++ admin/songlist.php
@@ -1,7 +1,7 @@
 <?php
 require_once( 'includes/gob_admin.php' );
 require_once( 'includes/admin_header.php' );
-require_once( 'includes/secrets.php' );
+require_once( '../src/secrets.php' );
 
 mod_check();
 ?>
@@ -9,7 +9,7 @@ mod_check();
 <h1>Song List</h1>
 <?php
 mysql_connect("localhost", $mysql_user, $mysql_password) or die(mysql_error());
-mysql_select_db("xxxdatabasexxx") or die(mysql_error());
+mysql_select_db($mysql_db) or die(mysql_error());
 
 $result = mysql_query("SELECT * FROM songs ORDER BY round DESC") or die(mysql_error()); ?>
 
@@ -22,7 +22,6 @@ $result = mysql_query("SELECT * FROM songs ORDER BY round DESC") or die(mysql_er
 			<th>Lyrics</th>
 			<th>Music</th>
 			<th>Vocals</th>
-			<th>SongLyrics</th>
 			<th>votesSong</th>
 			<th>votesLyrics</th>
 			<th>votesMusic</th>
@@ -34,21 +33,26 @@ $result = mysql_query("SELECT * FROM songs ORDER BY round DESC") or die(mysql_er
 	<tbody>
 		<?php
 		while($row = mysql_fetch_array($result)){
-			echo "<tr>";
+			echo "<tr";
+				if($round != $row['round']) echo ' class="newround"';
+			echo ">";
 				echo "<td>" . $row['round'] . "</td>";
-				echo "<td>" . $row['name'] . "</td>";
-				echo "<td>" . $row['url'] . "</td>";
+				echo '<td title="' . $row['lyricsheet'] . '">' . $row['name'] . '</td>';
+				echo '<td><a href="' . $row['url'] . '" target="_blank">Listen</a></td>';
 				echo "<td>" . $row['lyrics'] . "</td>";
 				echo "<td>" . $row['music'] . "</td>";
 				echo "<td>" . $row['vocals'] . "</td>";
-				echo "<td>" . substr($row['lyricsheet'], 0, 15) . "</td>";
 				echo "<td>" . $row['votes'] . "</td>";
 				echo "<td>" . $row['lyricsvote'] . "</td>";
 				echo "<td>" . $row['musicvote'] . "</td>";
 				echo "<td>" . $row['vocalsvote'] . "</td>";
-				echo "<td>" . $row['winner'] . "</td>";
+				echo "<td>";
+					if($row['winner']) echo 'Yes!';
+				echo "</td>";
 				echo '<td><a href="editsong.php?id='.$row['id'].'">Edit</a></td>';
 			echo "</tr>";
+			
+			$round = $row['round'];
 		}
 		?>
 	</tbody>
