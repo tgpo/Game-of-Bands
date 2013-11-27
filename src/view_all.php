7cc7fe6e470e922653e26256b6a57211dc90d3fc
tgpo <tgpotgpo@gmail.com>
2013-11-27 06:54:47 -0500

    Change HTML for easier styling

--- src/view_all.php
+++ src/view_all.php
@@ -1,12 +1,12 @@
-<div class='header'>
-  Select individual rounds, songs or bandits to navigate. <br />
-  <a href='/hall_of_fame'>View Hall of Fame</a> <br />
-  <a href='/all_rounds'>View Rounds by Theme</a> <br />
+<h2>Game of Bands Song Library</h2>
+<p>Select individual rounds, songs or bandits to navigate.</p>
 
-  Viewing Complete Song Library:
-</div>
+<aside id="otherviews">
+  <a href='/hall_of_fame' class="halloffame">View Hall of Fame</a> <br />
+  <a href='/all_rounds' class="roundbytheme">View Rounds by Theme</a> <br />
+</aside>
 
-<div id='content'>
+<section id='songlist'>
 <?php
   require_once('query.php');
   
@@ -14,4 +14,4 @@
   $songs = $db->query('SELECT * FROM songs WHERE approved=1 ORDER by id DESC');
   display_songs($songs);
 ?>
-</div>
+</section>
