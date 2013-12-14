6a6995e474d21885aaf93ebeca25fee36943b02d
tgpo <tgpotgpo@gmail.com>
2013-12-14 16:55:37 -0500

    New Admin CSS styles

--- admin/index.php
+++ admin/index.php
@@ -23,39 +23,60 @@ define('INDEX', true);
 <body>
   <div class="header-container">
     <header class="wrapper clearfix">
-        <span id="showMessages"><img src="/images/ico.email.gif"></span>
-        <section id="messagesContainer" style="display:none; background: #fff; border: 1px solid #ccc; padding: 7px; position: absolute; right: 10px;"><?php require('src/messages.php'); ?></section>
-        <h1 class="title"><a href="/admin" class="home">Game of Bands Admin</a></h1>
-        <nav>
+      <h1 class="title left"><a href="/admin" class="home">Game of Bands Admin</a></h1>
+      <span id="showMessages"><img src="/images/ico.email.gif"></span>
+      <section id="messagesContainer"><?php require('src/messages.php'); ?></section>
+      <div class="clearfix"></div>
+    </header>
+  </div>
+  <div id="container">
+    <div id="page">
+      <nav id="leftnav">
+        <ul>
+          <li>
+            <h3>Mod Functions</h3>
+          </li>
+          <li>
             <ul>
                 <li><a href="index.php?view=songlist">Songs</a></li>
                 <li><a href="index.php?view=roundlist">Rounds</a></li>
-				        <li><a href="index.php?view=teamlist">Teams</a></li>
+                <li><a href="index.php?view=teamlist">Teams</a></li>
+            </ul>
+          </li>
+          <li>
+            <h3>Use /u/GameofBands</h3>
+          </li>
+          <li>
+            <ul>
                 <li><a href="index.php?view=postmessage">Post Message</a></li>
             </ul>
-            <div class="clearfix"></div>
-        </nav>
-    </header>
-  </div>
+          </li>
+        </ul>
+        <div class="clearfix"></div>
+      </nav>
 
-  <div class="main-container">
-    <?php
-      // Decide which data to display
-      switch ($_GET['view']) {
-        case 'addsong'          : include_once 'addsong.php';        break;
-        case 'editsong'         : include_once 'editsong.php';       break;
-        case 'editround'        : include_once 'editround.php';      break;
-        case 'roundlist'        : include_once 'roundlist.php';      break;
-        case 'songlist'         : include_once 'songlist.php';       break;
-        case 'postmessage'      : include_once 'postmessage.php';    break;
-		    case 'teamlist'         : include_once 'teamlist.php';       break;
-		    case 'addteam'          : include_once 'addteam.php';        break;
-		    case 'editteam'         : include_once 'editteam.php';       break;
-		    case 'inbox'            : include_once 'inbox.php';          break;
-		    case 'editcss'          : include_once 'editcss.php';        break;
-        default                 : include_once 'dashboard.php';      break;
-      }
-    ?>
-  </div>	
+      <div id="content">
+        <div id="page-content">
+          <?php
+            // Decide which data to display
+            switch ($_GET['view']) {
+              case 'addsong'          : include_once 'addsong.php';        break;
+              case 'editsong'         : include_once 'editsong.php';       break;
+              case 'editround'        : include_once 'editround.php';      break;
+              case 'roundlist'        : include_once 'roundlist.php';      break;
+              case 'songlist'         : include_once 'songlist.php';       break;
+              case 'postmessage'      : include_once 'postmessage.php';    break;
+              case 'teamlist'         : include_once 'teamlist.php';       break;
+              case 'addteam'          : include_once 'addteam.php';        break;
+              case 'editteam'         : include_once 'editteam.php';       break;
+              case 'inbox'            : include_once 'inbox.php';          break;
+              case 'editcss'          : include_once 'editcss.php';        break;
+              default                 : include_once 'dashboard.php';      break;
+            }
+          ?>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
