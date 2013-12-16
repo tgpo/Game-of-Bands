186acef8d43a2a1135387fbfc9e389e96228afbc
tgpo <tgpotgpo@gmail.com>
2013-12-15 23:20:45 -0500

    Separate message code and convert to PDO statements

--- admin/src/messages.php
+++ admin/src/messages.php
@@ -1,17 +1,18 @@
 <?php
-if(!defined('INDEX')) {
-	header('Location: ../index.php');
-	die;
+if( !defined('INDEX') ) {
+    header( 'Location: ../index.php' );
+    die;
+
 }
 
 
 function displayMessages($currentuser){
-  $db = database_connect();
-  $messages = $db->prepare('SELECT COUNT(*) FROM messages WHERE user_to=:currentuser AND new = 1 order by date_sent desc');
-  $messages->execute(array('currentuser' => $currentuser));
+    $db = database_connect();
+    $messagecount = $db->prepare('SELECT COUNT(*) FROM messages WHERE user_to=:currentuser order by date_sent desc');
+    $messagecount->execute(array('currentuser' => $currentuser));
 
-  if ($messages->fetchColumn() > 0) {
-    $messages = $db->prepare('SELECT * FROM messages WHERE user_to=:currentuser AND new = 1 order by date_sent desc');
+    if( $messagecount->fetchColumn() > 0 ) {
+    $messages = $db->prepare('SELECT * FROM messages WHERE user_to=:currentuser order by date_sent desc');
     $messages->execute(array('currentuser' => $currentuser));
 
     foreach ($messages as $message) {
@@ -21,6 +22,7 @@ function displayMessages($currentuser){
   
       echo ">" . $message['body'] . "<br /><small>From: </small>" . $message['user_from'] . " <small>Sent: </small>" . $message['date_sent'] . "<br /><a href='#' class='delete'>Delete</a></li>";
     }
+
   } else {
     echo '<li id="noMessages">No Messages</li>';
   }
@@ -44,8 +46,9 @@ function displayBanditDropdown(){
   $selectHTML .= '</select>';
   
   echo $selectHTML;
+  
 }
-	
+
 ?>
 
 <script>
@@ -54,38 +57,38 @@ $(document).ready(function(){
       $('#messagesContainer').slideToggle();
     })
     $("#messages #postMessage").click(function(event){
-	      event.stopPropagation();
-	  
-	      var user_to = $('#messages #user_to').val();
-	      var user_from = "<? echo $_SESSION['GOB']['name'] ?>";
-	      var body = $(this).prev('#messageBody').find("#body").val();
-	  
-	      $.ajax({
-            url: 'admin_process.php',
-		        data: {postMessage: 'postMessage', user_to: user_to, user_from: user_from, body: body},
-		        type: 'post',
-		        success: function(output) {
+        event.stopPropagation();
+
+        var user_to = $('#messages #user_to').val();
+        var user_from = "<? echo $_SESSION['GOB']['name'] ?>";
+        var body = $(this).prev('#messageBody').find("#body").val();
+
+        $.ajax({
+            url: 'src/messages/process.php',
+            data: {postMessage: 'postMessage', user_to: user_to, user_from: user_from, body: body},
+            type: 'post',
+            success: function(output) {
                 if(user_to == "<? echo $_SESSION['GOB']['name'] ?>" || user_to == "allmods" || user_to == "everyone") {
                     var messageHTML =  '<li style="display:none;" data-id="' + output  + '" class="new justAdded">' + body + "<br /><small>From: </small>" + user_from + " <small>Sent: </small> <?php echo date('Y-m-d') ?>  <br /><a href='#' class='delete'>Delete</a></li>";
   
-				            $("#messagelist").prepend(messageHTML);
+                    $("#messagelist").prepend(messageHTML);
                     $("#messagelist li.justAdded").fadeIn().removeClass("justAdded");
-				            $('#messages #user_to').val("allmods");
-				            $('#messages #body').val("");
+                    $('#messages #user_to').val("allmods");
+                    $('#messages #body').val("");
                     if($('#noMessages')){
                         $('#noMessages').remove();
                     }
                     $('#messageCount').text( parseInt($('#messageCount').text()) + 1);
-			          };
-		        }
-	      });
-	  
-	      return false;
+                };
+            }
+        });
+
+        return false;
     });
-	
+
     $("#messagelist").on("click", "a.delete", function(event){
-	      event.stopPropagation();
-	      $(this).parent().fadeOut(300, function() {
+        event.stopPropagation();
+        $(this).parent().fadeOut(300, function() {
             if(($("#messagelist li").length - 1) == 0) {
                 $("#messagelist").prepend('<li id="noMessages">No Messages</li>');
             }
@@ -96,48 +99,48 @@ $(document).ready(function(){
 
             $(this).remove();
         });
-	  
-	      var messageID = $(this).parent().attr('data-id');
 
-	      $.ajax({
-            url: 'admin_process.php',
-		        data: {deleteMessage: 'deleteMessage', id: messageID},
-		        type: 'post',
-		        success: function(output) {
-		        }
-	      });
-	  
-	      return false;
+        var messageID = $(this).parent().attr('data-id');
+
+        $.ajax({
+            url: 'src/messages/process.php',
+            data: {deleteMessage: 'deleteMessage', id: messageID},
+            type: 'post',
+            success: function(output) {
+            }
+        });
+
+        return false;
     });
     
     $("#messagelist").on("click", "li", function(){
-	      $(this).removeClass('new');
-	      var messageID = $(this).attr('data-id');
-	      $.ajax({
-            url: 'admin_process.php',
-		        data: {action: 'markMessageRead', id: messageID},
-		        type: 'post',
-		        success: function(output) {
+        $(this).removeClass('new');
+        var messageID = $(this).attr('data-id');
+        $.ajax({
+            url: 'src/messages/process.php',
+            data: {markMessageRead: 'markMessageRead', id: messageID},
+            type: 'post',
+            success: function(output) {
               $('#messageCount').text( parseInt($('#messageCount').text()) - 1);
-        		}
-	      });
+            }
+        });
     });
 
 });
 </script>
-  <div id="messages">
+<div id="messages">
     <h2>Messages</h2>
-	  <ul id="messagelist">
-	    <?php displayMessages($_SESSION['GOB']['name']); ?>
-	  </ul>
-	  <h5>Post New Message</h5>
-	  <div id="messageTo">
-      <label for="user_to">To</label>
-	    <?php displayBanditDropdown(); ?>
+    <ul id="messagelist">
+        <?php displayMessages($_SESSION['GOB']['name']); ?>
+    </ul>
+    <h5>Post New Message</h5>
+    <div id="messageTo">
+        <label for="user_to">To</label>
+        <?php displayBanditDropdown(); ?>
     </div>
     <div id="messageBody">
-	    <textarea id="body" rows="15" cols="25" name="body"></textarea>
+        <textarea id="body" rows="15" cols="25" name="body"></textarea>
     </div>
 
-	  <button id="postMessage">Post Message</button>
-  </div>
+    <button id="postMessage">Post Message</button>
+</div>
---
+++ admin/src/messages/process.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once( '../../includes/gob_admin.php' );
+require_once( '../../../src/secrets.php' );
+require_once( '../../../src/query.php' );
+
+if( isset($_POST['postMessage']) ){
+  postMessage();
+
+} elseif ( isset($_POST['markMessageRead']) ) {
+  readMessage();
+
+} elseif( isset($_POST['deleteMessage']) ){
+  deleteMessage();
+
+}
+
+function postMessage(){
+    $db = database_connect();
+    $to = $_POST["user_to"];
+    $from = $_POST["user_from"];
+    $body = $_POST["body"];
+    $date = date('Y-m-d');
+
+    switch ($to) {
+        case "allmods":
+            $result = $db->query('SELECT * FROM bandits WHERE is_mod = 1');
+
+            while($bandits = mysql_fetch_array($result)){
+                $user_to = $bandits['name'];
+
+                $messages = $db->prepare('INSERT INTO messages (user_to, user_from, body, date_sent) VALUES (:to, :from, :body, :date)');
+                $messages->execute(array('to' => $to, 'from' => $from, 'body' => $body, 'date' => $date));
+            }
+
+            break;
+
+        case "everyone":
+            $result = $db->query('SELECT * FROM bandits');
+
+            while($bandits = mysql_fetch_array($result)){
+                $user_to = $bandits['name'];
+
+                $messages = $db->prepare('INSERT INTO messages (user_to, user_from, body, date_sent) VALUES (:to, :from, :body, :date)');
+                $messages->execute(array('to' => $to, 'from' => $from, 'body' => $body, 'date' => $date));
+            }
+
+            break;
+
+        default:
+            $messages = $db->prepare('INSERT INTO messages (user_to, user_from, body, date_sent) VALUES (:to, :from, :body, :date)');
+            $messages->execute(array('to' => $to, 'from' => $from, 'body' => $body, 'date' => $date));
+    }
+
+    echo $db->lastInsertId();
+
+}
+
+function readMessage(){
+    $db = database_connect();
+    $id = $_POST['id'];
+
+    $messages = $db->prepare('UPDATE messages SET new = :new WHERE id = :id');
+    $messages->execute(array('new' => false, 'id' => $id));
+
+}
+
+function deleteMessage(){
+    $db = database_connect();
+    $id = $_POST['id'];
+
+    $messages = $db->prepare('DELETE FROM messages WHERE id = :id');
+    $messages->execute(array('id' => $id));
+
+}
+
+?>
