3ff1b9bc6c0022a1d157a9239fdb1a48801d7bfb
IAmTriumph <Daniel.Edminster@gmail.com>
2013-12-21 18:39:46 -0500

    Update gob_user.php
    Slightly more extensive user menu.

--- src/gob_user.php
+++ src/gob_user.php
@@ -48,8 +48,12 @@ function write_karma(){
 
 }
 
-function get_bandit_link(){
-    return '<a href="/bandit/' . get_username() . '">' . get_username() . '</a>';
+function get_bandit_links(){
+    $links = get_username() . get_karma();
+    $links .= ' | ' . '<a href="/bandit/';
+    $links .=  get_username() . '">' . 'My Profile' . '</a>';
+    $links .= ' | ' . '<a href="/user_submitsong">' . 'Submit Song' . '</a>' . ' | ';
+    return $links;
 
 }
 
