fd7cfe11b9d3cc0ae3a5623611bf18cd26301095
IAmTriumph <Daniel.Edminster@gmail.com>
2014-01-02 14:45:39 -0500

    Fixed PDO inconsistency, added $bandit variable

--- edit_profile_process.php
+++ edit_profile_process.php
@@ -7,11 +7,12 @@
     $scu = filter_input(INPUT_POST, 'scu', FILTER_SANITIZE_URL);
     $url = filter_input(INPUT_POST, 'url', FILTER_SANITIZE_URL);
     $gear = filter_input(INPUT_POST, 'gear', FILTER_SANITIZE_SPECIAL_CHARS);
+    $bandit = get_username();
 
 	$db = database_connect();
-	$query = $db->prepare('UPDATE bandits SET soundcloud_url=:scu, website=:url, tools=:gear WHERE name="'  .  get_username()  .  '"');
-	$query->execute(array('scu' => $scu,'url' => $url,'gear' => $gear);
-	header('Location: /bandit/'  .  get_username());
+	$query = $db->prepare('UPDATE bandits SET soundcloud_url=:scu, website=:url, tools=:gear WHERE name=:bandit');
+	$query->execute(array('scu' => $scu,'url' => $url,'gear' => $gear,'bandit' => $bandit);
+	header('Location: /bandit/'  .  $bandit);
 
 ?>
 
