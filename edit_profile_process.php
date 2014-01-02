0b468dbccb142920e89d22193b92e2b670d5197e
IAmTriumph <Daniel.Edminster@gmail.com>
2014-01-01 21:30:56 -0500

    process profile edits
    sanitizes input & writes to db

---
+++ edit_profile_process.php
@@ -0,0 +1,18 @@
+<?php
+    require_once('src/query.php');
+    require_once('src/gob_user.php');
+
+    loggedin_check();
+	        
+    $scu = filter_input(INPUT_POST, 'scu', FILTER_SANITIZE_URL);
+    $url = filter_input(INPUT_POST, 'url', FILTER_SANITIZE_URL);
+    $gear = filter_input(INPUT_POST, 'gear', FILTER_SANITIZE_SPECIAL_CHARS);
+
+	$db = database_connect();
+	$query = $db->prepare('UPDATE bandits SET soundcloud_url=:scu, website=:url, tools=:gear WHERE name="'  .  get_username()  .  '"');
+	$query->execute(array('scu' => $scu,'url' => $url,'gear' => $gear);
+	header('Location: /bandit/'  .  get_username());
+
+?>
+
+			
