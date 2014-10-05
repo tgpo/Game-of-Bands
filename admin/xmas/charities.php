9e63782f67fb749f8a48179b411c9bfd7f795e60
Aaron Were <clonemeagain@gmail.com>
2014-10-05 14:02:43 +1100

    Created charities & teams files

---
+++ admin/xmas/charities.php
@@ -0,0 +1,5 @@
+//TODO: display list of nominated charities, their vote-counts,
+		 and the teams associated <br />
+//TODO: include functions to contact the charity 
+	<a href=\"https://github.com/clonemeagain/Game-of-Bands/issues/8\">Issue in question</a>
+	<br />
