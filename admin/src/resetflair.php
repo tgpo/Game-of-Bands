e5b6a123c5da0f59d096586603f9892ac35e360d
Aaron Were <clonemeagain@gmail.com>
2014-10-11 13:41:07 +1100

    Adding output on reset flair interface, makes it obvious what is happening.

--- admin/src/resetflair.php
+++ admin/src/resetflair.php
@@ -17,36 +17,43 @@ $query = $db->query('SELECT * FROM rounds WHERE theme IS NOT NULL AND theme <> "
 $round = $query->fetch();
 $currentround = $round['number'];
 
+echo '<h1>Current round: ' . $currentround . '</h1><p>Note: Number of queries required depends on number of rounds, this may take a minute.</p>';
+
 //Reset Counts
-$query = $db->query('UPDATE bandits SET TeamWins = NULL, Musicwins = NULL, Lyricswins = NULL, Vocalswins = NULL WHERE 1');
+$query = $db->query('UPDATE bandits SET TeamWins = NULL, Musicwins = NULL, Lyricswins = NULL, Vocalswins = NULL');
 
 //Dev Overide
 $currentround = $currentround-1;
 
+$out = array();
+
 for ($i = 1; $i <= $currentround; $i++) {
 
   $winningSongs = query_songs($i, $db);
-  foreach ($winningSongs as $song) {
+  foreach ($winningSongs as $song) {	
+  	$out[]= 'Found: ' . count($winningSongs) . ' winning songs.<br />';
     if (!empty($song['votes'])) {
       $banditTypes = array('music','lyrics','vocals');
       foreach ($banditTypes as $type) {
+      	$out[]= 'Winning ' . $type . 'votes song = ' . $song['name'] . '<br />';
         //Make sure bandit exists in bandit table
-        $query = $db->query("SELECT * FROM bandits WHERE name = '$song[$type]'");
+        $query = $db->query("SELECT * FROM bandits WHERE name = '$song[$type]' LIMIT 1");
         $bandit = $query->fetch();
-        
-        
 
          if(!$bandit['name']){
-          $query = $db->query("INSERT INTO bandits (name, is_mod, banned) VALUES ('$song[$type]', 0, 0)");
+         	$out[]= '<strong>Added bandit: ' . $winner[$type] . '</strong> -----------> Yay!<br />';
+          	$query = $db->query("INSERT INTO bandits (name, is_mod, banned) VALUES ('$song[$type]', 0, 0)");
         }
         
-        $query = $db->prepare('SELECT TeamWins FROM bandits WHERE name=:bandit');
+        $query = $db->prepare('SELECT TeamWins FROM bandits WHERE name=:bandit LIMIT 1');
         $query->execute(array('bandit' => $song[$type]));
         $bandit  = $query->fetch();
 
         $TeamWins = $bandit['TeamWins']+1;
+        
+       $out[]= 'Setting ' . $TeamWins . ' wins for ' . $song[$type] . '<br />';
 
-        $query = $db->prepare('UPDATE bandits SET TeamWins = :TeamWins WHERE name=:bandit');
+        $query = $db->prepare('UPDATE bandits SET TeamWins = :TeamWins WHERE name=:bandit LIMIT 1');
         $query->execute(array('TeamWins' => $TeamWins, 'bandit' => $song[$type]));
         
       }
@@ -63,25 +70,31 @@ for ($i = 1; $i <= $currentround; $i++) {
           $bandit = $query->fetch();
           
           if(!$bandit['name']){
-            $query = $db->query("INSERT INTO bandits (name, is_mod, banned) VALUES ('$winner[$type]', 0, 0)");
+          	$out[]= '<strong>Added bandit: ' . $winner[$type] . '</strong> -----------> Yay!<br />';
+            $query = $db->query("INSERT INTO bandits (name, is_mod, banned) VALUES ('$winner[$type]', 0, 0) LIMIT 1");
           }
           
           $currentfield = ucfirst($type) . 'Wins';
 
-          $query = $db->prepare('SELECT ' . $currentfield . ' FROM bandits WHERE name=:bandit');
+          $query = $db->prepare('SELECT ' . $currentfield . ' FROM bandits WHERE name=:bandit LIMIT 1');
           $query->execute(array('bandit' => $winner[$type]));
           $bandit  = $query->fetch();
 
           $wins = $bandit[$currentfield]+1;
+          
+          $out[]= 'Setting ' . $type . ' to ' . $wins . ' for ' . $winner[$type] . '<br />';
 
-          $query = $db->prepare('UPDATE bandits SET ' . $currentfield . ' = :Wins WHERE name=:bandit');
+          $query = $db->prepare('UPDATE bandits SET ' . $currentfield . ' = :Wins WHERE name=:bandit LIMIT 1');
           $query->execute(array('Wins' => $wins, 'bandit' => $winner[$type]));
         }
       }
   }
-  
+  $out[]= '<br /><h2>Resetting flair for round: ' . $i . '</h2>';
   
 }
+//print array, invert the output so its obvious.
+foreach(array_reverse($out) as $l){echo $l;}
+
 
 // Query winning songs
 function query_songs($round, $db) {
@@ -100,7 +113,7 @@ function query_songs($round, $db) {
         LIMIT 1  
     )";
 
-    $result = $db->query($query);
+    $result = sql_to_array($query);
 
     return $result;
 }
@@ -122,10 +135,7 @@ function query_winners($type, $round, $db) {
         LIMIT 1  
     )";
 
-    $result = $db->query($query);
+    $result = sql_to_array($query);
 
     return $result;
 }
-?>
-
-
